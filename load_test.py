3bc5d1c34cb4daba901b3c05c532ec26411dd91e
chyn00 <ybj0749@gmail.com>
2025-07-11 11:37:58 +0900

    update. 정확한 집계를 위해 wait_time변경

--- load_test.py
+++ load_test.py
@@ -1,8 +1,16 @@
-from locust import HttpUser, task, between
+from locust import FastHttpUser, task, between, constant_pacing
+import random
+import uuid
 
-class MyUser(HttpUser):
-  wait_time = between(1, 3)
+class OrderLoadTest(FastHttpUser):
+  wait_time = constant_pacing(1)  # 1초 마다의 정확한 집계를 위해 변경
 
   @task
-  def index(self):
-    self.client.get("/")
+  def order_stock_limit(self):
+    payload = {
+      "itemCode": "1234",
+      "userId": str(uuid.uuid4()),
+      "quantity": 1
+    }
+
+    self.client.post("/order/stock/limit", json=payload)
